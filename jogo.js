5f8abff3ffce32567716dba0ebcc5cf4fff21ea9
Lucas Campos <bhlucascampos@gmail.com>
2022-10-06 21:50:36 -0300

    update 0.1

---
+++ jogo.js
@@ -0,0 +1,16 @@
+﻿var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo() {
+    var altura = window.innerHeight
+    var largura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+var posicaoX = Math.random() * largura
+var posicaoY = Math.random() * altura
+
+console.log(posicaoX, posicaoY)
