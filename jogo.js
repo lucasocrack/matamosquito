41927c8a588226bf9c7a8ef3fc80680777129c56
Lucas Campos <bhlucascampos@gmail.com>
2022-10-07 10:30:03 -0300

    update base game

--- jogo.js
+++ jogo.js
@@ -1,16 +1,74 @@
 ﻿var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
 
     console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
-var posicaoX = Math.random() * largura
-var posicaoY = Math.random() * altura
+function posicaoRandonMosquito() {
 
-console.log(posicaoX, posicaoY)
+    //remover o mosquito anterior antes de gerar um novo
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            alert('Acabou as vidas')
+        } else {
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
+            vidas++
+        }
+    }
+
+    var posicaoX = Math.round(Math.random() * largura) - 90
+    var posicaoY = Math.round(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criar img randon
+    var mosquito = document.createElement('img')
+    mosquito.src = "imagens/mosquito.png"
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function () {
+        this.remove()
+    }
+
+    document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
